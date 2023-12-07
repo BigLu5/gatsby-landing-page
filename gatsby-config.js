5b2e0c78d72a70f9a5bc153fa96d6cc0a1bfb1dc
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-12-07 11:46:06 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Zap Marketing`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
