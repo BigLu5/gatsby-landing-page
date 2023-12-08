dc92a140f3d9683d8aaffcbcf1c147775e256d24
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-12-08 21:50:20 +0000

    add responsive design to header burger menu currently cannot close once opened

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,16 +3,30 @@ import Container from "../container";
 import * as styles from "./styles.module.scss";
 
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className={styles.navbar}>
+    <div className={`${styles.navbar} ${isMenuOpen ? styles.menuOpen : ""}`}>
       <Container>
         <div className={styles.navbarInner}>
           <div className={styles.navbarLeft}>
             <a className={styles.logo} href="#">
               Zap Marketing
             </a>
-
-            <ul className={styles.list}>
+          </div>
+          <div className={styles.navbarRight}>
+            <div className={styles.hamburger} onClick={toggleMenu}>
+              <div className={styles.bar}></div>
+              <div className={styles.bar}></div>
+              <div className={styles.bar}></div>
+            </div>
+            <ul
+              className={`${styles.list} ${isMenuOpen ? styles.menuOpen : ""}`}
+            >
               <li className={styles.listItem}>
                 <a href="#">About</a>
               </li>
